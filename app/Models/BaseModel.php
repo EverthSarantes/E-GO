ce355b9c7f4384864c8abe1996b4debcbe1b3395
Everth Sarantes <everthleonardosaran@gmail.com>
2025-07-30 20:53:38 -0600

    feat: add BaseModel as a base class for other models

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+    //Este modelo se utiliza como base para otros modelos de la aplicación.
+}
