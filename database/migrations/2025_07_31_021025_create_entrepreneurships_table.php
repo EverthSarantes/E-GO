e8730d73008bbd80af8ccd4ef069d2bdf86c0137
Everth Sarantes <everthleonardosaran@gmail.com>
2025-07-30 20:19:27 -0600

    feat: create migrations for entrepreneurships table

---
+++ database/migrations/2025_07_31_021025_create_entrepreneurships_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('entrepreneurships', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('years_of_experience')->default(0);
+            $table->text('description')->nullable();
+
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->foreign('department_id')->references('id')->on('departments')
+            ->onDelete('set null')->onUpdate('cascade');
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onDelete('set null')->onUpdate('cascade');
+
+            $table->unsignedBigInteger('entrepreneurship_type_id')->nullable();
+            $table->foreign('entrepreneurship_type_id')->references('id')->on('entrepreneurships_types')
+            ->onDelete('set null')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('entrepreneurships');
+    }
+};
