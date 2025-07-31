ab36caf57271ae10cba8058b789e4934b5fe62b2
Everth Sarantes <everthleonardosaran@gmail.com>
2025-07-30 21:59:24 -0600

    feat: add customer and entrepreneur panel views

---
+++ resources/views/customer/panel.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Panel de Administración')
+
+@section('content')
+    <div class="container">
+        <h1>Panel de Cliente</h1>
+    </div>
+@endsection
---
+++ resources/views/entrepreneur/panel.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Panel de Administración')
+
+@section('content')
+    <div class="container">
+        <h1>Panel de Emprendedor</h1>
+    </div>
+@endsection
